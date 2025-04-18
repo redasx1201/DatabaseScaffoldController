ae0cb970205f4eacfb04ea5a06b7a9f529afbf7b
redasx1201 <ashtonxu1201@gmail.com>
2025-04-18 17:03:46 -0600

    IDK

--- LMSHandout/LMS/obj/Debug/net9.0/LMS.AssemblyInfo.cs
+++ LMSHandout/LMS/obj/Debug/net9.0/LMS.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94373064879ba28934643580d1fea011e433c8f")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d3bd7bc67e2908567cb6d0607c51d981549104")]
+>>>>>>> parent of c943730 (testing needed)
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

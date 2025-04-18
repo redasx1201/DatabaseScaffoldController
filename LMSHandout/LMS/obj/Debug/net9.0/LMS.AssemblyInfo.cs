f3f58c7124563a983916a117157a8d9ae82e0094
zkreu <zackkreulen@gmail.com>
2025-04-18 14:38:36 -0600

    implemented controllers-need testing

--- LMSHandout/LMS/obj/Debug/net9.0/LMS.AssemblyInfo.cs
+++ LMSHandout/LMS/obj/Debug/net9.0/LMS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53280605f3cb13b9f648de903f3a6a822dce10ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2fc7d19169803508c329a26833f962035d548d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
